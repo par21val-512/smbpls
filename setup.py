a0176630628f485fe66e7fbbf4c4283ef0fe9337
par21val-512 <161367215+par21val-512@users.noreply.github.com>
2026-03-14 14:02:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="smbpls",  # this is the pip / import name
     version="0.1.0",
     description="Sparse Multi-Block Partial Least Squares for multi-omics MuData",
-    author="Your Name",
+    author="Ray Zhang",
     packages=find_packages(),
     python_requires=">=3.9",
     install_requires=[
